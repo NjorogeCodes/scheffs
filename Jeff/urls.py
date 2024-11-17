ae3743d9925bbb51ce2524e62ed946c297b03084
NjorogeCodes <nestahke01@gmail.com>
2024-11-17 04:24:13 +0300

    first commit

--- Jeff/urls.py
+++ Jeff/urls.py
@@ -4,12 +4,17 @@ from .import views
 
 urlpatterns=[
     path('',views.home,name='index'),
-    path('about/',views.about,name='about'),
-    path('contact/',views.contact,name='contact'),
+    path('about/',views.about,name='aboutit'),
+    path('services/', views.services, name='services'),
     path('menu/',views.menu,name='menu'),
-    path('services/',views.services,name='services'),
-    path('team/',views.team,name='team'),
-    path('testimonials/',views.testimonial,name='testimonial'),
     path('booking/',views.booking,name='booking'),
+    path('team/',views.team,name='team'),
+    path('testimonial/',views.testimonial,name='testimonial'),
+    path('contact/',views.contact,name='contact'),
+
+
+
+
+
 
 ]
--- Jeff/views.py
+++ Jeff/views.py
@@ -5,15 +5,20 @@ def home(request):
     return render(request, 'index.html')
 def about(request):
     return render(request, 'about.html')
-def contact(request):
-    return render(request, 'contact.html')
-def booking(request):
-    return render(request, 'booking.html')
-def menu(request):
-    return render(request, 'menu.html')
 def services(request):
     return render(request, 'service.html')
+def menu(request):
+    return render(request, 'menu.html')
+def booking(request):
+    return render(request, 'booking.html')
 def team(request):
     return render(request, 'team.html')
 def testimonial(request):
     return render(request, 'testimonial.html')
+def contact(request):
+    return render(request, 'contact.html')
+
+
+
+
+
